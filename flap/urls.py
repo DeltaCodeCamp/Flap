000680e57048d9444e5e2a40199b1a0c73b9b891
Anish Basnet <anishbasnet840@gmail.com>
2019-06-28 14:48:55 +0545

    all registration done SurajJha

--- flap/urls.py
+++ flap/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -6,7 +6,9 @@ urlpatterns = [
 path('signin', views.signin, name = "signin"),
 path('special_second', views.special_second, name = "special_second"),
 path('organization_second', views.organization_second, name = "organization_second"),
-path('special', views.special, name = "special"),
-path('organization', views.organization, name = "organization"),
-path('events', views.events, name = "events"),
+path('special', views.special_, name = "special"),
+path('organization', views.organization_, name = "organization"),
+path('events', views.events_, name = "events"),
+path('home', views.home, name = "home"),
+re_path(r'^activation/(?P<redirection_code>[\w.@+-]+)/$', views.activation, name = "activation")
 ]
